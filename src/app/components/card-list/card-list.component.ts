fb4264ab5780b2ce87d52d0e968ab28c22c24355
Matheus Marcelo <matheusmarcelo314@gmail.com>
2023-12-27 21:14:35 -0300

    create unit cards list

---
+++ src/app/components/card-list/card-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '../../types/location.interface';
+
+@Component({
+  selector: 'app-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrl: './card-list.component.scss'
+})
+export class CardListComponent implements OnInit {
+  @Input() unitsList: Location[] = [];
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+  }
+}
