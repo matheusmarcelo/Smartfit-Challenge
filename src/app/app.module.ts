db69c44ae1a871036f86a9535c30a37c197f73dd
Matheus Marcelo <matheusmarcelo314@gmail.com>
2023-12-27 21:21:49 -0300

    add legend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,22 @@ import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
+import { LegendsComponent } from './components/legends/legends.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormsComponent
+    FormsComponent,
+    CardListComponent,
+    CardComponent,
+    LegendsComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
---
+++ src/app/components/legends/legends.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legends',
+  templateUrl: './legends.component.html',
+  styleUrl: './legends.component.scss'
+})
+export class LegendsComponent {
+
+}
